45ce534001ef2de88b9ddc48efb50da3db769494
Ashish Gupta <ashish.gupta06@nagarro.com>
2023-05-02 13:54:04 +0530

    Initialized repo

---
+++ src/main/java/io/github/logicbeat/dupicheck/DupicheckApplication.java
@@ -0,0 +1,13 @@
+package io.github.logicbeat.dupicheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DupicheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DupicheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/logicbeat/dupicheck/DupicheckApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.logicbeat.dupicheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DupicheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
